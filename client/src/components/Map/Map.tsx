a678eccfe98b84eb03a3a1ca826ae4a3eaf2b05c
unknown <gergoalbert.gta@gmail.com>
2020-08-20 17:22:54 +0300

    Get current position

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { UserLocationInfo } from "../../interfaces"
 import styled from "styled-components";
 import mapboxgl from "mapbox-gl";
 
@@ -7,17 +8,25 @@ const MapContainer = styled.section`
 `;
 
 const Map = () => {
-  mapboxgl.accessToken =
-    "pk.eyJ1Ijoid29kbnlhbiIsImEiOiJja2UydWJuZW0wY3NxMzduN2ZwOGIxdm9lIn0.xNPhlTTsb9LY3LDXiAJ9sw";
+  const [location, setLocation] = useState<UserLocationInfo>({ long: 0, lat: 0 });
   const mapRef = useRef<any>(null);
+  mapboxgl.accessToken = "pk.eyJ1Ijoid29kbnlhbiIsImEiOiJja2UydWJuZW0wY3NxMzduN2ZwOGIxdm9lIn0.xNPhlTTsb9LY3LDXiAJ9sw";
+  //Draw map
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapRef.current,
       style: "mapbox://styles/mapbox/dark-v10",
-      center: [30.5, 50.5],
-      zoom: 1,
+      center: [location.long, location.lat],
+      zoom: location.long !== 0 ? 15 : 0,
     });
-  }, [mapRef]);
+  }, [mapRef, location]);
+
+  //Get user location
+  useEffect(() => {
+    fetch("http://ip-api.com/json/")
+      .then((resp: any) => resp.json())
+      .then((resp: any) => setLocation({lat: resp.lat, long: resp.lon}))
+  }, []);
   return (
     <>
       <MapContainer ref={mapRef} />
