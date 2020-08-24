859813e9e05e6946ed188768b18b3fc5f2f9b674
Wodnyan <wodnyan@pop-os.localdomain>
2020-08-24 10:22:49 +0300

    Menu change animation

--- client/src/components/Menu/MenuList.tsx
+++ client/src/components/Menu/MenuList.tsx
@@ -1,24 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
+import { CSSTransition } from "react-transition-group";
 import * as S from "../../styles/Menu";
 
 interface MenuListItem {
   children: React.ReactNode;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
-const MenuListItem: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const MenuListItem: React.FC<MenuListItem> = ({ children, onClick }) => {
   return (
-    <S.MenuListItem>{children}</S.MenuListItem>
+    <S.MenuListItem onClick={onClick}>{children}</S.MenuListItem>
   )
 }
 
 const MenuList = () => {
+  const [menuState, setMenuState] = useState("all");
   return (
-    <S.MenuList>
-      <MenuListItem>Hello world</MenuListItem>
-      <MenuListItem>Hello world</MenuListItem>
-      <MenuListItem>Settings</MenuListItem>
-    </S.MenuList>
+    <S.Menu>
+      <CSSTransition
+        in={menuState === "all"}
+        timeout={300}
+        classNames="primary"
+        unmountOnExit
+      >
+        <S.MenuList>
+          <MenuListItem>Hello world</MenuListItem>
+          <MenuListItem>Hello world</MenuListItem>
+          <MenuListItem onClick={() => setMenuState("settings")}>Settings</MenuListItem>
+        </S.MenuList>
+      </CSSTransition>
+      <CSSTransition
+        in={menuState === "settings"}
+        timeout={300}
+        classNames="secondary"
+        unmountOnExit
+      >
+        <S.MenuList>
+          <MenuListItem onClick={() => setMenuState("all")}>Go Back</MenuListItem>
+          <MenuListItem>Hello world</MenuListItem>
+        </S.MenuList>
+      </CSSTransition>
+    </S.Menu>
   )
 }
 export default MenuList
