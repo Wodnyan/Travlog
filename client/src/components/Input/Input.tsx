bfbd4cfb51eaea8a412addb3fdd0bddd0caeb3ce
Albert Gergo <gergoalbert.gta@gmail.com>
2020-08-26 19:15:21 +0300

    Added a floating label component

--- client/src/components/Input/Input.tsx
+++ client/src/components/Input/Input.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import * as S from "../../styles/Input";
+import React from "react";
+import * as S from "../../styles/Form";
 
 interface Input {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -7,26 +7,44 @@ interface Input {
   name: string;
 }
 
-export const PasswordInput: React.FC<Input> = ({
-  onChange,
-  value,
-  name
-}) => {
-  return (
-    <>
-      <S.Input name={name} type="password" onChange={onChange} value={value}/>
-    </>
-  )
+interface Label {
+  children: React.ReactNode;
+  label: string;
 }
 
-export const TextInput: React.FC<Input> = ({
-  onChange,
-  value,
-  name
-}) => {
+export const PasswordInput: React.FC<Input> = ({ onChange, value, name }) => {
   return (
     <>
-      <S.Input name={name} type="text" onChange={onChange} value={value}/>
+      <S.Input
+        placeholder=" "
+        name={name}
+        type="password"
+        onChange={onChange}
+        value={value}
+      />
     </>
-  )
-}
+  );
+};
+
+export const TextInput: React.FC<Input> = ({ onChange, value, name }) => {
+  return (
+    <>
+      <S.Input
+        placeholder=" "
+        name={name}
+        type="text"
+        onChange={onChange}
+        value={value}
+      />
+    </>
+  );
+};
+
+export const FloatingLabel: React.FC<Label> = ({ label, children }) => {
+  return (
+    <S.Label>
+      {children}
+      <div className="label__text">{label}</div>
+    </S.Label>
+  );
+};
--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,42 +1,59 @@
-import React, { useReducer } from 'react'
-import { inputReducer } from "../reducers"; 
-import { TextInput, PasswordInput } from "../components/Input/Input";
+import React, { useReducer } from "react";
+import { inputReducer } from "../reducers";
+import {
+  TextInput,
+  PasswordInput,
+  FloatingLabel,
+} from "../components/Input/Input";
+import { Button } from "../styles/Button";
+import { Form } from "../styles/Form";
 
 const inputInitArgs = {
   username: "",
   password: "",
-  erro: "",
-  isLoading: false
-}
+  error: "",
+  isLoading: false,
+};
 
 const Login = () => {
   const [input, inputDispatch] = useReducer(inputReducer, inputInitArgs);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    inputDispatch({ type: "login" });
+  };
+
   return (
-    <div>
-      <TextInput
-        name="username" 
-        onChange={
-          (e: React.ChangeEvent<HTMLInputElement>) => inputDispatch({
-            type: "field",
-            field: e.target.name,
-            targetValue: e.target.value
-          })
-        } 
-        value={input.username}
-      />
-      <PasswordInput 
-        name="password" 
-        onChange={
-          (e: React.ChangeEvent<HTMLInputElement>) => inputDispatch({
-            type: "field",
-            field: e.target.name,
-            targetValue: e.target.value
-          })
-        } 
-        value={input.password}
-      />
-    </div>
-  )
-}
+    <Form onSubmit={handleSubmit}>
+      <FloatingLabel label="username">
+        <TextInput
+          name="username"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            inputDispatch({
+              type: "field",
+              field: e.target.name,
+              targetValue: e.target.value,
+            })
+          }
+          value={input.username}
+        />
+      </FloatingLabel>
+      <FloatingLabel label="password">
+        <PasswordInput
+          name="password"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            inputDispatch({
+              type: "field",
+              field: e.target.name,
+              targetValue: e.target.value,
+            })
+          }
+          value={input.password}
+        />
+      </FloatingLabel>
+      <Button type="submit">Login</Button>
+    </Form>
+  );
+};
 
 export default Login;
