62015e84021cc46cc996c5615b72b45f7a725d55
Albert Gergo <gergoalbert.gta@gmail.com>
2020-08-29 15:52:37 +0300

    General layout of the login page

--- client/src/styles/Global.tsx
+++ client/src/styles/Global.tsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
   *,
@@ -12,5 +12,21 @@ export const GlobalStyles = createGlobalStyle`
   body,
   #root{
     height: 100%;
+    background: grey;
   }
-`
+`;
+export const SplitInTwoVertical = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-content: center;
+`;
+
+export const ImageContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  img {
+    object-fit: fill;
+  }
+`;
