646de6dac969fa49ffb6cffe30a3ed728197f773
Albert Gergo <gergoalbert.gta@gmail.com>
2020-08-30 20:38:56 +0300

    Local Auth

--- backend/src/auth/auth.ts
+++ backend/src/auth/auth.ts
@@ -1,33 +1,65 @@
 import { Router } from "express";
+import { User } from "../db/db";
+import bcrypt from "bcrypt";
 import passport from "passport";
 
 const router = Router();
 const CLIENT_URL = "http://localhost:3000";
 
 router.get("/github", passport.authenticate("github"));
-router.get("/github/callback", passport.authenticate("github", {
-  failureRedirect: "/",
-  successRedirect: CLIENT_URL
-}));
+router.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/",
+    successRedirect: CLIENT_URL,
+  })
+);
 
 router.get("/facebook", passport.authenticate("facebook"));
-router.get("/facebook/callback", passport.authenticate("facebook", {
-  failureRedirect: "/",
-  successRedirect: CLIENT_URL
-}));
+router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", {
+    failureRedirect: "/",
+    successRedirect: CLIENT_URL,
+  })
+);
+
+router.get(
+  "/local",
+  passport.authenticate("local", {
+    successRedirect: CLIENT_URL,
+  })
+);
+router.post("/local/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      username: username,
+      password: hashedPassword,
+      provider: "local",
+    });
+    res.json({
+      message: "Successful register",
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/login", (req, res) => {
-  if(req.user) {
+  if (req.user) {
     res.json(req.user);
   } else {
     res.json({
-      error: "Something went wrong"
-    })
+      error: "Something went wrong",
+    });
   }
-})
+});
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect(CLIENT_URL);
-})
+});
 
 export default router;
--- backend/src/config/passport-config.ts
+++ backend/src/config/passport-config.ts
@@ -2,9 +2,10 @@ import passport from "passport";
 import dotenv from "dotenv";
 import passportGithub, { Strategy } from "passport-github";
 import passportFacebook from "passport-facebook";
+import passportLocal from "passport-local";
+import bcrypt from "bcrypt";
 import { User } from "../db/db";
 import { UserTypes } from "../types";
-import { PromiseProvider } from "mongoose";
 
 dotenv.config();
 
@@ -19,6 +20,26 @@ function passportConfig() {
 
   const GithubStrategy = passportGithub.Strategy;
   const FacebookStrategy = passportFacebook.Strategy;
+  const PassportLocalStrategy = passportLocal.Strategy;
+
+  passport.use(
+    new PassportLocalStrategy(async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username });
+        if (user === null) {
+          throw new Error("Couldn't find user");
+        }
+        // @ts-ignore
+        const dehashedPassword = await bcrypt.compare(password, user.password);
+        if (!dehashedPassword) {
+          throw new Error("Incorrect Password");
+        }
+        done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    })
+  );
 
   passport.use(
     new GithubStrategy(
