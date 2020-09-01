6317bfcf78be4806c00f9903ef31c2883366602f
Albert Gergo <gergoalbert.gta@gmail.com>
2020-09-01 19:35:24 +0300

    Found a react mapbox package and going to use that

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { GlobalStyles } from "./styles/Global";
-import Map from "./components/Map/Map";
+import Map from "./views/Map";
 import LoginPage from "./views/Login";
 import SignUpPage from "./views/SignUp";
 
--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,51 +1,5 @@
-import React, { useState, useRef, useEffect } from "react";
-import { UserLocationInfo } from "../../types";
-import styled from "styled-components";
-import mapboxgl from "mapbox-gl";
-import Menu from "../Menu/Menu";
+import React from "react";
 
-const Container = styled.div`
-  position: relative;
-  height: 100%;
-`
-
-const Nav = styled.nav`
-  position: absolute;
-  bottom: 2rem;
-  right: 2rem;
-`
-
-const MapContainer = styled.section`
-  height: 100%;
-`;
-
-const Map = () => {
-  const [location, setLocation] = useState<UserLocationInfo>({ long: 0, lat: 0 });
-  const mapRef = useRef<any>(null);
-  mapboxgl.accessToken = "pk.eyJ1Ijoid29kbnlhbiIsImEiOiJja2UydWJuZW0wY3NxMzduN2ZwOGIxdm9lIn0.xNPhlTTsb9LY3LDXiAJ9sw";
-  //Draw map
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapRef.current,
-      style: "mapbox://styles/mapbox/dark-v10",
-      center: [location.long, location.lat],
-      zoom: location.long !== 0 ? 15 : 0,
-    });
-  }, [mapRef, location]);
-
-  //Get user location
-  useEffect(() => {
-    fetch("http://ip-api.com/json/")
-      .then((resp: any) => resp.json())
-      .then((resp: any) => setLocation({lat: resp.lat, long: resp.lon}))
-  }, []);
-  return (
-    <Container>
-      <MapContainer ref={mapRef} />
-      <Nav>
-        <Menu />
-      </Nav>
-    </Container>
-  );
+export const Map = () => {
+  return <div></div>;
 };
-export default Map;
