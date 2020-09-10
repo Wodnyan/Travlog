116278386f3359e70575984e769f050954231fbd
Albert Gergo <gergoalbert.gta@gmail.com>
2020-09-10 12:42:59 +0300

    Minor style changes

--- client/src/styles/Button.tsx
+++ client/src/styles/Button.tsx
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const Button = styled.button``;
+export const Button = styled.button`
+  padding: 0.5rem 4rem;
+  border: blue 2px solid;
+  border-radius: 7px;
+  background: transparent;
+  font-size: 1rem;
+  color: #fff;
+  cursor: pointer;
+`;
